584c4d6e1fcc8e2dc8e6aa1febb4cf3e65c2147a
manuelcamaioni <camaioni.manuel@gmail.com>
2023-08-30 15:29:40 +0200

    added api request in appmain.vue

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,16 +1,40 @@
 <template>
     <main>
-        Main
+        <div class="card">
+
+        </div>
     </main>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
     data() {
         return {
-
+            apiUrl: 'http://127.0.0.1:8000/api/projects',
+            projects: []
         }
     },
+
+    methods: {
+        getProjects() {
+            axios.get(this.apiUrl)
+                .then((response) => {
+                    // handle success
+                    console.log(response.data.results.data);
+                    this.projects = response.data.results.data
+                })
+                .catch(function (error) {
+                    // handle error
+                    console.log(error);
+                });
+
+        }
+
+    },
+    created() {
+        this.getProjects();
+    }
 }
 </script>
 
