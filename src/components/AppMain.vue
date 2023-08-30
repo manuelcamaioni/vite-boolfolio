979df3d8351c965f7c9c1f40888eb92c848a331c
manuelcamaioni <camaioni.manuel@gmail.com>
2023-08-30 16:20:28 +0200

    added cards to main

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,7 +1,28 @@
 <template>
     <main>
-        <div class="card">
+        <div class="container-fluid d-flex flex-wrap justify-content-between">
+            <div class="card mb-3" v-for="project in projects">
+                <div class="card-header">
+                    <img :src="project.image" class="card-img-top" alt="not found">
+                </div>
 
+                <div class="card-body">
+                    <h5 class="card-title">{{ project.title }}</h5>
+
+                    <p class="card-text">
+                        {{ project.description }}
+                    </p>
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">{{ project.type.name }}</li>
+                    <li class="list-group-item" v-if="project.technologies.length > 0">
+                        <span v-for="technology in project.technologies">{{ technology.name }}</span>
+                    </li>
+                    <li class="list-group-item" v-else>No technology registered.</li>
+                    <li class="list-group-item"><i>{{ project.date }}</i></li>
+                </ul>
+
+            </div>
         </div>
     </main>
 </template>
@@ -29,7 +50,8 @@ export default {
                     console.log(error);
                 });
 
-        }
+        },
+
 
     },
     created() {
@@ -38,4 +60,16 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+div.card {
+    width: calc(100% / 3 - 1rem);
+
+    .card-header {
+        padding: 0;
+    }
+
+    ul {
+        list-style-type: none;
+    }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from "vue";
 import "./styles/general.scss";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.min.js";
 import App from "./App.vue";
 
 createApp(App).mount("#app");
