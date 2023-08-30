034ea7897413ec628e2c7aa89450749651d179e2
manuelcamaioni <camaioni.manuel@gmail.com>
2023-08-30 15:20:45 +0200

    setup project vite+vue

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,17 @@
+<template>
+    <footer>
+        Footer
+    </footer>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    },
+}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,17 @@
+<template>
+    <header>
+        Header
+    </header>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    },
+}
+</script>
+
+<style lang="scss" scoped></style>
