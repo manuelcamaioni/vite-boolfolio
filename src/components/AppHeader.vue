95f0b76392206be5eed2e917ccf9ab768963ed2e
manuelcamaioni <camaioni.manuel@gmail.com>
2023-08-31 14:53:04 +0200

    refactored navbar

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,12 +1,27 @@
 <template>
     <header>
+
+        <nav class="navbar navbar-expand-lg bg-light">
+            <div class="container-fluid">
+                <router-link class="navbar-brand" :to="{ name: 'homepage' }">BoolFolio</router-link>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li v-for="link in links" class="nav-item">
+                            <router-link :to="{ name: link.route }" class="nav-link">
+                                {{ link.name }}
+                            </router-link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
         <nav>
             <ul>
-                <li v-for="link in links">
-                    <router-link :to="{ name: link.route }">
-                        {{ link.name }}
-                    </router-link>
-                </li>
+
             </ul>
         </nav>
     </header>
@@ -35,4 +50,11 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+header {
+    li {
+
+        list-style-type: none;
+    }
+}
+</style>
