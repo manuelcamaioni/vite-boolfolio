3a15a0239e814109c113887a1aaea48d9c6e89fd
manuelcamaioni <camaioni.manuel@gmail.com>
2023-08-31 16:43:29 +0200

    added postshow route to router.js

--- src/router.js
+++ src/router.js
@@ -2,11 +2,13 @@ import { createRouter, createWebHistory } from "vue-router";
 import Homepage from "./components/views/Homepage.vue";
 import AboutUs from "./components/views/AboutUs.vue";
 import AppMain from "./components/AppMain.vue";
-// import ProjectShow from "";
+import ProjectShow from "./components/views/ProjectShow.vue";
+
 const routes = [
     { path: "/", name: "homepage", component: Homepage },
     { path: "/about", name: "about", component: AboutUs },
     { path: "/projects", name: "projects-list", component: AppMain },
+    { path: "/projects/:id", name: "show-project", component: ProjectShow },
 ];
 
 const router = createRouter({
